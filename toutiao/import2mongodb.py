b7b458f3e2ec5e701ae9b65ce48e0baf2d9c3d36
pmh905001 <pmh905001@126.com>
2023-12-20 15:22:19 +0800

    incremental import to mongodb if user have new favorites files

--- toutiao/import2mongodb.py
+++ toutiao/import2mongodb.py
@@ -5,19 +5,48 @@ import pymongo
 
 
 def import_to_mongodb():
-    my_favorite_files = sorted([f for f in os.listdir('.') if f.startswith('myfavorites-')])
-    for file_name in my_favorite_files:
+    last_id = find_last_id_from_db()
+    my_favorite_files = get_my_favorites_files(last_id)
+    for file_name, pos in my_favorite_files:
         print(f'------------------------{file_name}')
         with open(file_name, 'r', encoding='utf-8') as f:
             lines = f.readlines()[::-1]
+            if pos != -1:
+                lines = lines[-pos:]
             for line in lines:
                 page: dict = json.loads(line)
                 records = page['data']
                 if records:
                     records = records[::-1]
-                    # for r in records:
-                    #     print(r)
                     write_to_db(records)
+
+
+def get_my_favorites_files(last_id):
+    files = sorted([f for f in os.listdir('.') if f.startswith('myfavorites-')])
+    if not last_id:
+        return [(f, -1) for f in files]
+    files = sorted(files, reverse=True)
+    result = []
+    for file in files:
+        with open(file, 'r', encoding='utf-8') as f:
+            for line in f:
+                page = json.loads(line)
+                if page['data']:
+                    for index, record in enumerate(page['data']):
+                        if record['id'] == last_id:
+                            if index != 0:
+                                result.append((file, index))
+                            return result[::-1]
+            result.append((file, -1))
+    raise FileNotFoundError(f'not found files by id: {last_id}')
+
+
+def find_last_id_from_db():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["myfav"]
+    collections = db["toutiao"]
+    record = collections.find().sort({'_id': -1}).limit(1).next()
+    return record['id'] if record else None
 
 
 def write_to_db(records):
