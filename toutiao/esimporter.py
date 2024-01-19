ed5930411bcc4ed747fc90e13dc64f726f7b76f4
pmh905001 <pmh905001@126.com>
2024-01-19 16:06:36 +0800

    bulk insert records to elasticsearch

--- toutiao/esimporter.py
+++ toutiao/esimporter.py
@@ -33,7 +33,7 @@ class ESImporter(DBImporter):
     def find_last_id_from_db(self):
         return self.id
 
-    def write_to_db(self, records):
+    def write_to_db_slow(self, records):
         for record in records:
             self.increasement_id += 1
             print(f"------------------------increasement_id:{self.increasement_id}")
@@ -44,6 +44,48 @@ class ESImporter(DBImporter):
                 self.client.index(index=self.index, body=record)
             except:
                 logging.exception(f'ignore this error record\n:{json.dumps(record, indent=4, ensure_ascii=False)}')
+
+
+    def write_to_db(self, records):
+        records_to_insert=[]
+        for record in records:
+            self.increasement_id += 1
+            print(f"------------------------increasement_id:{self.increasement_id}")
+            record['increasement_id'] = self.increasement_id
+            if 'content' in record and not isinstance(record['content'], str):
+                record['content'] = json.dumps(record['content'], ensure_ascii=False)
+            records_to_insert.append(
+                {
+                    "_index": self.index,
+                    "_source": record
+                }
+            )
+        from elasticsearch import helpers
+        helpers.bulk(self.client, records_to_insert)
+
+
+    def import_to_db(self):
+        last_id = self.find_last_id_from_db()
+        files = self._files()
+        my_favorite_files = self._locate_position(files, last_id)
+        for file_name, line_number, record_number in my_favorite_files:
+            print(f'------------------------{file_name}')
+            with open(file_name, 'r', encoding='utf-8') as f:
+                lines = f.readlines()[::-1]
+                if line_number != -1:
+                    lines = lines[-line_number:]
+                total_records=[]
+                for line in lines:
+                    page: dict = json.loads(line)
+                    records = page['data']
+                    if records:
+                        records = records[::-1]
+                        if record_number != -1:
+                            records = records[-record_number:]
+                    total_records.extend(records)
+                self.write_to_db(total_records)
+
+
 
 
 if __name__ == '__main__':
