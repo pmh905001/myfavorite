2bac5db0ff383152883eedd212539e69bb1baf37
pmh905001 <pmh905001@126.com>
2024-01-23 22:37:50 +0800

    if key word is not inputed,list last 10 records

--- toutiao/essearcher.py
+++ toutiao/essearcher.py
@@ -1,29 +1,40 @@
+import logging
+
 from prettytable import PrettyTable
 
 from toutiao.es import ES
 
 
 class ESSearcher(ES):
+    SIZE = 10
 
     def __init__(self, url='http://localhost:9200', index="mytoutiaofav"):
         super().__init__(url, index)
 
-    def query(self, keywords=None):
-        if not keywords:
-            return None
+    def query(self, keywords=None, page=1, size=10):
         body = {
-            # source means author.
-            "query": {"multi_match": {"query": f"{keywords}", "fields": ["title", 'abstract', 'source', 'content']}},
-            # "query": {"multi_match": {"query": f"{keywords}", "fields": ["title"]}},
-            # should be most matched, not only order by increasement_id
             "sort": [{"_score": {"order": "desc"}}, {"increasement_id": {"order": "desc", 'unmapped_type': 'long'}}],
-            "size": 20
+            "from": (page - 1) * size,
+            "size": size
         }
+        if keywords:
+            # source means author.
+            body.update(
+                {
+                    "query": {
+                        "multi_match": {
+                            "query": f"{keywords}",
+                            "fields": ["title", 'abstract', 'source', 'content']
+                        }
+                    }
+                }
+            )
         result = self.client.search(index=self.index, body=body, ignore_unavailable=True)
         return result
 
     def display(self, response):
         hits = response['hits']['hits']
+        logging.getLogger().setLevel(logging.INFO)
         table = PrettyTable(['Title', 'Link', 'increasement_id', '_score'])
         for record in hits:
             source = record['_source']
@@ -31,11 +42,13 @@ class ESSearcher(ES):
             table.add_row(row)
         print(table)
 
-    def search(self, keywords):
-        response = self.query(keywords)
+    def search(self, keywords, page=1, size=SIZE):
+        response = self.query(keywords, page, size)
         self.display(response)
 
 
 if __name__ == '__main__':
     # ESSearcher().search('python 内存管理')
-    ESSearcher(url='http://192.168.3.185:9200').search('python 内存管理')
+    # ESSearcher(url='http://192.168.3.185:9200').search('python 内存管理')
+    # ESSearcher(url='http://192.168.3.185:9200').search('')
+    ESSearcher().search('', 3, 100)
