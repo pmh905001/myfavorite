bfa0c38541b4049308750e932c95ed4c69790fb5
pmh905001 <pmh905001@126.com>
2024-10-03 00:24:49 +0800

    fix delete issue

--- toutiao/apiservice.py
+++ toutiao/apiservice.py
@@ -3,27 +3,48 @@ import json
 from flask import Flask, request, make_response, render_template
 
 from essearcher import ESSearcher
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__, template_folder='../views')
 
 
 @app.route('/search', methods=['GET'])
+# @cross_origin()
 def search():
     keywords = request.args.get('keywords')
     page = int(request.args.get('page', '1'))
     result = ESSearcher().query(keywords, page).raw['hits']['hits']
-    result = [x['_source'] for x in result]
+    
+    # result = [x['_source'] for x in result]
+    records=[]
+    for x in result:
+        source =x['_source']
+        source["_id"]=x["_id"]
+        records.append(source)
+        
     # return json.dumps(result, ensure_ascii=False), 200, {'Content-Type': 'application/json'}
-    resp = make_response(json.dumps(result, ensure_ascii=False))
+    resp = make_response(json.dumps(records, ensure_ascii=False))
     resp.status = '200'
     resp.headers['Access-Control-Allow-Origin'] = "*"
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
 @app.route('/remove', methods=['DELETE'])
+@cross_origin()
 def delete():
-    id = request.args.get('id')
+    id = request.args.get('_id')
+    print(f'--------------------------{id}')
     ESSearcher().delete(id)
+    # resp = make_response(json.dumps({}, ensure_ascii=False))
+    # resp.status = '200'
+    # resp.headers['Access-Control-Allow-Origin'] = "*"
+    # resp.headers['Content-Type'] = 'application/json'
+    # return resp
+    return ""
+    
+    
+    
 
 @app.route('/myfavs', methods=['GET'])
 def myfavs():
